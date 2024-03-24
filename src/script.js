7fcfc3bd7c8d668e5e48e84e006bd0d5e02c7c36
Jullin-01 <tsvetkova_93@mail.ru>
2024-03-24 18:06:16 +0300

    added platforms in gameScene

--- src/script.js
+++
@@ -1,8 +0,0 @@
-import {Launcher} from '../src/launcher/launcher.js'
-import './styles.css';
-
-let _APP = null;
-
-window.addEventListener('DOMContentLoaded', () => {
-    _APP = new Launcher();
-}); 
